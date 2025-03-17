7d2f48cb4119df687d1fd367ce0d3430eae045cc
maiquoctuan <tuan.mq.63cntt@ntu.edu.vn>
2025-03-17 12:54:25 +0700

    Capnhat

---
+++ LoginScreen/app/src/main/java/mqtuan/loginscreen/MainActivity.java
@@ -0,0 +1,35 @@
+package mqtuan.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("admin") && pass.equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
